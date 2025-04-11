8c2c2170b035ca37ab0887f97c629460e0c1a35c
omar-momen <omarelzhery236@gmail.com>
2025-04-11 14:05:03 +0200

    disableAllBranches service

--- src/services/branches.ts
+++ src/services/branches.ts
@@ -1,6 +1,16 @@
-import type { BranchesResponse } from '@/types/branches'
+import type { BranchesResponse, Branch } from '@/types'
 import useApi from '@/composables/useApi'
 
 export const getBranches = () => {
   return useApi().get<BranchesResponse>('/branches?include[0]=sections&include[1]=sections.tables')
+}
+
+export const updateBranch = (branchId: string, data: Partial<Branch>) => {
+  return useApi().put(`/branches/${branchId}`, data)
+}
+
+export const disableAllBranches = async (branches: Branch[]) => {
+  return Promise.all(
+    branches.map((branch) => updateBranch(branch.id, { accepts_reservations: false })),
+  )
 }
